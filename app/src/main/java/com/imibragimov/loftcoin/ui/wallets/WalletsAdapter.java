da7c27f8127498c632c21768121bc6e6d87c88f0
imibrm <imibrm@mail.ru>
2021-05-29 17:57:37 +0300

    Week1 Homework 1 and 2

---
+++ app/src/main/java/com/imibragimov/loftcoin/ui/wallets/WalletsAdapter.java
@@ -0,0 +1,45 @@
+package com.imibragimov.loftcoin.ui.wallets;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.imibragimov.loftcoin.databinding.LiWalletBinding;
+
+class WalletsAdapter extends RecyclerView.Adapter<WalletsAdapter.ViewHolder> {
+
+    private LayoutInflater inflater;
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LiWalletBinding.inflate(inflater, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        inflater = LayoutInflater.from(recyclerView.getContext());
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ViewHolder(@NonNull LiWalletBinding binding) {
+            super(binding.getRoot());
+        }
+
+    }
+
+}
